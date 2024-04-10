39d48489350d88ac6517018a1d579419ec626c6d
SAM <SAM@AORUS>
2024-04-10 10:21:09 +0530

    Add project files.

---
+++ FightGame/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FightGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(150);
+            int level = 1;
+            int numberOfEnemies = 1;
+
+            BossEnemy boss = new BossEnemy(50);
+            while (level <= 6 && player.Health > 0)
+            {
+                Level currentLevel = new Level(player, level, numberOfEnemies);
+                currentLevel.Play();
+
+
+
+                level++;
+                if (level <= 5)
+                {
+                    numberOfEnemies++;
+                }
+            }
+
+
+        }
+    }
+}
